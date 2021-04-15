8a43cf81338ed363e9fecb0146c1ab4ee171b7bb
ched88xtc <ched88official@gmail.com>
2021-04-15 19:19:48 +0300

    Header with adaptive and mobile version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.querySelector('.header__burgermenu')
+  .addEventListener('click', function() {
+    document.querySelector('.header__burgermenu-icon').classList
+      .toggle('active');
+    document.querySelector('.header__menu-wrapper').classList.toggle('animate');
+    document.querySelector('.header__girl-wrapper').classList.toggle('animate');
+  });
